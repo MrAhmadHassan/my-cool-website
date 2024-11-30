20ea64ee36a5dd431c523e9814e87f20252d587f
Ahmad Hassan <ahmadhassan478626@gmail.com>
2024-11-30 18:12:20 +0500

    portfolio app starter

---
+++ script.js
@@ -0,0 +1,6 @@
+// GSAP Animations
+gsap.from("header", { duration: 1, y: -50, opacity: 0 });
+gsap.from("#about", { duration: 1, x: -100, opacity: 0 });
+gsap.from("#tech-stack", { duration: 1, scale: 0.5, opacity: 0 });
+gsap.from("#projects", { duration: 1, y: 100, opacity: 0 });
+gsap.from("footer", { duration: 1, y: 50, opacity: 0 });
